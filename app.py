3fca5343efda60c548774b354aa536feb2e7a5b2
Hugotovio <95087048+Hugotovio@users.noreply.github.com>
2026-01-30 07:21:43 -0500

    ajuste cors

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ CORS(
             "origins": [
                 "http://localhost:5000",
                 "http://127.0.0.1:5000",
-                "https://web-production-0153.up.railway.app/"
+                "https://web-production-0153.up.railway.app"
             ],
             "methods": ["POST"],
             "allow_headers": ["Content-Type"]
@@ -107,7 +107,7 @@ def calcular_densidad():
 # ❤️ HEALTH CHECK
 # ==============================
 @app.route("/health", methods=["GET"])
-def health():
+def health(): lo
     return jsonify({"status": "ok"})
 
 # ==============================
